cb68b49ad78dc900e58315cb6e45a65dec5f244e
EdwardVizcarraVH <Wasabiacme2@gmail.com>
2026-03-16 11:18:06 -0400

    Programa sin comentarios

--- programa_par.java
+++ programa_par.java
@@ -4,14 +4,12 @@ public class Programa_par {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.print("Introduce un número entero: ");
+        System.out.print("Introduce un número entero gaaaaaaaa: ");
         int numero = teclado.nextInt();
-
-        // La condición mágica
         if (numero % 2 == 0) {
-            System.out.println("El número " + numero + " es PAR.");
+            System.out.println("El número " + numero + " es PAR OMG.");
         } else {
-            System.out.println("El número " + numero + " es IMPAR.");
+            System.out.println("El número " + numero + " es IMPAR OMG.");
         }
         
         teclado.close();
